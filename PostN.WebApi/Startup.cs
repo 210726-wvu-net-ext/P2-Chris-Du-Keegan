1911a6c2ee6e1f267557fa6b5c91174527ea1b70
kwedwick <64446581+kwedwick@users.noreply.github.com>
2021-09-11 12:05:54 -0500

    repos and db is linked

--- PostN.WebApi/Startup.cs
+++ PostN.WebApi/Startup.cs
@@ -2,11 +2,15 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using PostN.DataAccess;
+using PostN.DataAccess.Entities;
+using PostN.Domain;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -28,8 +32,14 @@ namespace PostN.WebApi
         {
 
             //REPOS GO HERE
-            //services.AddScoped<IUserRepo, UserRepo>();
-            //services.AddScoped<IPostRepo, PostRepo>();
+            services.AddScoped<IUserRepo, UserRepo>();
+            services.AddScoped<IPostRepo, PostRepo>();
+
+            services.AddDbContext<CMKWDTP2Context>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("p2-210726-cdk"));
+                options.LogTo(Console.WriteLine);
+            });
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
