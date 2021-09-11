c5d5bb827204205a183da0dcc708e1f801cd0121
dutran <lovesandlife@gmail.com>
2021-09-11 15:33:52 -0400

    add UserController

---
+++ PostN.Domain/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace PostN.Domain
+{
+    public class User
+    {
+        public User() { }
+       
+        public User(int id, string firstName, string lastName, string aboutMe)
+        {
+            this.Id = id;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.AboutMe = aboutMe;
+        }
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public string AboutMe { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public string Email { get; set; }
+        public int PhoneNumber { get; set; }
+        public DateTime Dob { get; set; }
+        
+        public string Profilepic { get; set; }
+    }
+}
---
+++ PostN.WebApi/Controllers/UserController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PostN.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PostN.WebApi.Controllers
+{
+    
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly ILogger<UserController> _logger;
+        private readonly IUserRepo _repo;
+        public UserController(ILogger<UserController> logger, IUserRepo repo)
+        {
+            _logger = logger;
+            _repo = repo;
+        }
+        // GET: api/<UserController>
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            var user = _repo.GetUsers();
+            return Ok(user);
+        }
+
+        // GET api/<UserController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<UserController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<UserController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<UserController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
