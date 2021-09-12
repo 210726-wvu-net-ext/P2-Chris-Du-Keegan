9f6e032e6c83b5b1671c04ede23e29dc153705c9
kwedwick <64446581+kwedwick@users.noreply.github.com>
2021-09-12 14:18:39 -0500

    PostController setup. Just have the repos

--- PostN.Domain/IPostRepo.cs
+++ PostN.Domain/IPostRepo.cs
@@ -4,10 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace PostN.Domain
 {
     public interface IPostRepo
     {
+        List<Post> GetAllPosts();
+        Post GetPostById(int id);
 
+        Post CreatePost(Post post);
+
+        Post UpdatePostById(int id, Post post);
+
+        Post DeletePostById(int id, Post post);
+
+        Post CreateCommentByPostId(int postId, Comment comment);
+
+        Post UpdateCommentById(int commentId, Comment comment);
+
+        Post DeleteCommentById(int commentId, Comment comment);
     }
 }
