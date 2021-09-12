a76c456517272386f97456d10ca62e4cf8f8104c
kwedwick <64446581+kwedwick@users.noreply.github.com>
2021-09-12 14:25:06 -0500

    PostRepo started

--- PostN.DataAccess/PostRepo.cs
+++ PostN.DataAccess/PostRepo.cs
@@ -9,5 +9,44 @@ namespace PostN.DataAccess
 {
     public class PostRepo : IPostRepo
     {
+        public Post CreateCommentByPostId(int postId, Comment comment)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Post CreatePost(Post post)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Post DeleteCommentById(int commentId, Comment comment)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Post DeletePostById(int id, Post post)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Post> GetAllPosts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Post GetPostById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Post UpdateCommentById(int commentId, Comment comment)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Post UpdatePostById(int id, Post post)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
