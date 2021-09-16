17a842eb2ecfe379fd85397dce8b47a23b1f69da
kwedwick <64446581+kwedwick@users.noreply.github.com>
2021-09-16 12:08:38 -0500

    updated user returned info to include comments and posts

--- PostN.Domain/IUserRepo.cs
+++ PostN.Domain/IUserRepo.cs
@@ -10,9 +10,9 @@ namespace PostN.Domain
     public interface IUserRepo
     {
         Task<List<User>> GetUsers();
-        Task<User> GetUsersById(int id);
+        Task<User> GetUserById(int id);
         Task<User> UpdateUser(int id, string otherFirstName, string otherLastName, string otherEmail, string otherPhoneNumber, string otherAboutMe);
-        User SearchUserById(int id);
+        Task<User> SearchUserById(int id);
         Task<User> AddAUser(User user);
         Task<bool> DeleteUserById(int id);
         List<Follower> GetFollowers();
