10754296db011cd6a4d09649323f016c55ae1814
kwedwick <64446581+kwedwick@users.noreply.github.com>
2021-09-18 16:02:07 -0500

    updated items

--- PostNApp-UI/src/app/home/home.component.ts
+++ PostNApp-UI/src/app/home/home.component.ts
@@ -14,6 +14,18 @@ export class HomeComponent {
   posts: Post[] = [];
   constructor(private jwtHelper: JwtHelperService, private router: Router, private postService: PostService) {}
 
+  returnUserId() {
+    const token: string = JSON.parse(localStorage.getItem("jwt")!);
+    const userId: string = this.jwtHelper.decodeToken(token).NameIdentifier;
+    if (token && !this.jwtHelper.isTokenExpired(token)) {
+      alert(userId);
+      return userId;
+    }
+    else {
+      return null;
+    }
+  }
+
   isUserAuthenticated() {
     const token: string = JSON.parse(localStorage.getItem("jwt")!);
     if (token && !this.jwtHelper.isTokenExpired(token)) {
@@ -25,6 +37,7 @@ export class HomeComponent {
   }
   ngOnInit(): void {
     this.getPosts();
+    this.returnUserId();
   }
   getPosts(): void {
     this.postService.getPosts()
--- PostNApp-UI/src/app/posts/posts.component.ts
+++ PostNApp-UI/src/app/posts/posts.component.ts
@@ -1,19 +1,21 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Post } from '../interfaces/post';
 
+
+
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.css']
 })
 export class PostsComponent implements OnInit {
-
+  panelOpenState = false;
+  
   @Input() post?: Post;
 
   constructor() { }
 
   ngOnInit(): void {
   }
-  
-
+ 
 }
