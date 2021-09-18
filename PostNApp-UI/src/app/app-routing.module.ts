df981c438534b14f8b12354c577c4d0d15940403
kwedwick <64446581+kwedwick@users.noreply.github.com>
2021-09-17 20:39:42 -0500

    created and added Post angular routes. Untests

--- PostNApp-UI/src/app/app-routing.module.ts
+++ PostNApp-UI/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { ProfileComponent } from './profile/profile.component';
 import { UserDetailComponent } from './user-detail/user-detail.component';
 import { LoginComponent } from './login/login.component';
 import { AuthGuard } from './guards/auth-gaurd.service';
+import { ExploreComponent } from './explore/explore.component';
 
 
 const routes: Routes = [
@@ -12,6 +13,7 @@ const routes: Routes = [
   { path: 'profile', component: ProfileComponent },
   { path: 'profile/update/:id', component: UserDetailComponent, canActivate: [AuthGuard] },
   { path: 'users', component: UsersComponent },
+  { path: 'explore', component: ExploreComponent},
   { path: 'login', component: LoginComponent}
 ];
 
