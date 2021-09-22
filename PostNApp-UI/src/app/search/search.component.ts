8b45d011659e393a78f1dbd737342e023cabba2a
Chris Mesidor <christopher.mesidor1996@gmail.com>
2021-09-22 12:25:39 -0400

    adding filter pipe back to code

--- PostNApp-UI/src/app/search/search.component.ts
+++ PostNApp-UI/src/app/search/search.component.ts
@@ -10,6 +10,7 @@ import { Location } from '@angular/common';
 })
 export class SearchComponent implements OnInit {
   users: User[] = [];
+  username: string = "";
   constructor(private location: Location,private userService: UserService) { }
 
   ngOnInit(): void {
