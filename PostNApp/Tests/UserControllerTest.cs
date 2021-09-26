97ccdd92e09bc7493e414a4cbe3ceef684312caa
kwedwick <64446581+kwedwick@users.noreply.github.com>
2021-09-26 17:44:09 -0500

    updated usercontroller

--- PostNApp/Tests/UserControllerTest.cs
+++ PostNApp/Tests/UserControllerTest.cs
@@ -20,6 +20,7 @@ namespace Tests
         public UserControllerTest()
         {
             options = new DbContextOptionsBuilder<CMKWDTP2Context>().UseSqlite("Filename=Test0.db").Options;
+            Seed();
         }
         [Fact]
         public async void GetAllUsersAsync_ShouldReturnAllUsers()
@@ -35,6 +36,183 @@ namespace Tests
                 var result = await controller.GetUsers();
                 var jsonResult = (ObjectResult)result.Result;
                 Assert.Equal(testUser.Result, (IEnumerable<User>)jsonResult.Value);
+            }
+        }
+
+        private void Seed()
+        {
+            using (var context = new CMKWDTP2Context(options))
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+                context.Users.AddRange(
+                    new Entity.User
+                    {
+                        Id = 1,
+                        FirstName = "Keegan",
+                        LastName = "Wedwick",
+                        Email = "kwedwick@gmail.com",
+                        Username = "kwedwick",
+                        Password = "password",
+                        AboutMe = "Keegan About Me",
+                        State = "WI",
+                        Country = "USA",
+                        Role = "Administrator",
+                        PhoneNumber = "608-479-1147",
+                        DoB = DateTime.Today
+                    }
+                    ,
+                    new Entity.User
+                    {
+                        Id = 2,
+                        FirstName = "Christopher",
+                        LastName = "Mesidor",
+                        Email = "cMesidor@gmail.com",
+                        Username = "cmesidor",
+                        Password = "password1234",
+                        AboutMe = "New .NET developer",
+                        State = "NY",
+                        Country = "USA",
+                        Role = "Administrator",
+                        PhoneNumber = "123-456-1234",
+                        DoB = DateTime.Today
+                    },
+                    new Entity.User
+                    {
+                        Id = 3,
+                        FirstName = "Du",
+                        LastName = "Traun",
+                        Email = "dTran@gmail.com",
+                        Username = "dTran",
+                        Password = "password",
+                        AboutMe = "New ASP.NET developer",
+                        State = "MA",
+                        Country = "USA",
+                        Role = "Administrator",
+                        PhoneNumber = "789-123-1111",
+                        DoB = DateTime.Today
+
+                    },
+                    new Entity.User
+                    {
+                        Id = 4,
+                        FirstName = "Elizabeth",
+                        LastName = "Jackson",
+                        Email = "eJackson@gmail.com",
+                        Username = "ejackson",
+                        Password = "password",
+                        AboutMe = "New JavasScript developer",
+                        State = "CA",
+                        Country = "USA",
+                        Role = "User",
+                        PhoneNumber = "399-555-1928",
+                        DoB = DateTime.Today
+
+                    }
+                );
+
+
+                context.Posts.AddRange(
+                    new Entity.Post
+                    {
+                        Id = 1,
+                        UserId = 1,
+                        Created = DateTime.Now,
+                        Title = "Keegan first title",
+                        Body = "This is Keegan''s post body!"
+                    },
+                    new Entity.Post
+                    {
+                        Id = 2,
+                        UserId = 2,
+                        Created = DateTime.Now,
+                        Title = "Chris first title",
+                        Body = "This is Chris''s post body!"
+                    },
+                    new Entity.Post
+                    {
+                        Id = 3,
+                        UserId = 3,
+                        Created = DateTime.Now,
+                        Title = "Du first title",
+                        Body = "This is Du''s post body!"
+                    },
+                    new Entity.Post
+                    {
+                        Id = 4,
+                        UserId = 4,
+                        Created = DateTime.Now,
+                        Title = "Elizabeth first title",
+                        Body = "This is Elizabeth''s post body!"
+                    }
+                ); ;
+
+                context.Comments.AddRange(
+                    new Entity.Comment
+                    {
+                        Id = 1,
+                        UserId = 2,
+                        PostId = 1,
+                        Created = DateTime.Now,
+                        CommentBody = "Great post Keegan! - Chris"
+                    },
+                    new Entity.Comment
+                    {
+                        Id = 2,
+                        UserId = 3,
+                        PostId = 2,
+                        Created = DateTime.Now,
+                        CommentBody = "Great post Chris! - Du"
+                    },
+                    new Entity.Comment
+                    {
+                        Id = 3,
+                        UserId = 4,
+                        PostId = 3,
+                        Created = DateTime.Now,
+                        CommentBody = "Great post Du! - Elizabeth"
+                    },
+                    new Entity.Comment
+                    {
+                        Id = 4,
+                        UserId = 1,
+                        PostId = 4,
+                        Created = DateTime.Now,
+                        CommentBody = "Great post Elizabeth! - Keegan"
+                    }
+                );
+
+                context.Followers.AddRange(
+                    new Entity.Follower
+                    {
+                        Id = 1,
+                        UserId = 1,
+                        UserId2 = 2,
+                        FriendRequest = 1,
+                    },
+                    new Entity.Follower
+                    {
+                        Id = 2,
+                        UserId = 2,
+                        UserId2 = 3,
+                        FriendRequest = 1,
+                    },
+                    new Entity.Follower
+                    {
+                        Id = 3,
+                        UserId = 3,
+                        UserId2 = 4,
+                        FriendRequest = 1,
+                    },
+                    new Entity.Follower
+                    {
+                        Id = 4,
+                        UserId = 4,
+                        UserId2 = 1,
+                        FriendRequest = 1,
+                    }
+                );
+                context.SaveChanges();
             }
         }
     }
